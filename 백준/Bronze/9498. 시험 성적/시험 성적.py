0d01556bc7a857578839bbe612bb2fe710078a57
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2022-09-23 14:50:36 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+Score = int(input())
+
+if 90<=Score:
+    print("A")
+elif 80<=Score<=89:
+    print("B")
+elif 70<=Score<=79:
+    print("C")
+elif 60<=Score<=69:
+    print("D")
+else:
+    print("F")
