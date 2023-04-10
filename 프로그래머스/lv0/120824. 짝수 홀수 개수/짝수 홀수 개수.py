71421ca9bb56a62de91c1261f587bc28c7575f7f
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-04-10 19:22:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = []
+    
+    odd_num, even_num = 0,0
+    for num in num_list:
+        if num % 2 ==0:
+            even_num += 1
+        else:
+            odd_num += 1
+            
+        answer = [even_num, odd_num]
+    return answer
