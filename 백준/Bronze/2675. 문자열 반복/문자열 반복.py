841d9862736e4df3b0eff91c9789f4564b48db1a
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-07-27 14:54:51 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,7 +1,14 @@
-t = int(input())
-for i in range(t):
-    num, s = input().split()
-    text = ''
-    for i in s:
-        text += int(num) * i
-    print(text)
+#몇번 할건지 입력받기
+
+#반복문으로 입력값 받기
+#형변환해준후
+#반복문으로 문자 찍어주기
+
+Count = int(input())
+
+for i in range(Count):
+    R,S = input().split()
+    R = int(R)
+    for i in range(len(S)):
+        print(R*S[i],end="")
+    print()
