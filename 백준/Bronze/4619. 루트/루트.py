5650d44729b6a6b90529a0146b96962191d855c2
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-07-28 15:47:40 +0900

    [Bronze III] Title: 루트, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4619. 루트/루트.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    b, n = map(int, input().split())
+    if b == n == 0:
+        break
+    i = 0
+    while i**n < b:
+        i += 1
+    print(i if i**n-b < b-(i-1)**n else i-1)
