abd0a9d6f2743eafb339c500440b264de7e2f30e
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-08-17 10:20:07 +0900

    [Bronze II] Title: 수 정렬하기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,15 +1,7 @@
-N = int(input())
-
-numbers = []
-
-for _ in range(N) : 
-    numbers.append(int(input()))
-
-# Bubble Sort
-for i in range(len(numbers)) : 
-    for j in range(len(numbers)) : 
-        if numbers[i] < numbers[j] : 
-            numbers[i], numbers[j] = numbers[j], numbers[i]
-            
-for n in numbers : 
-    print(n)
+cnt = int(input())
+num = []
+for i in range(cnt):
+    num.append(int(input()))
+num.sort()
+for i in num:
+    print(i)
