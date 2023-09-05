3864aa62889f47dbb1e3444af8e44e7a6a5015ac
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-05 13:39:16 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.py
@@ -1,8 +1,2 @@
 def solution(arr):
-    sum = 0
-    answer = 0
-    #리스트 안의 요소들의 총합 구하기
-    for i in range(len(arr)):
-        sum += arr[i]
-    return sum / len(arr)
-    # 총합 / 나누기 = 평균
+    return sum(arr) / len(arr)
