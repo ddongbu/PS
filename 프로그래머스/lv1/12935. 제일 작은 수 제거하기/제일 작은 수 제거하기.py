5e3d112eb1ff8e0b87ab8defbdf15b054134a815
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-11 14:39:45 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,6 +1,10 @@
 def solution(arr):
+    answer = []
     if len(arr) > 1:
         arr.remove(min(arr))
         return arr
     else:
         return [-1]
+    #arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수
+    #조건 -> 
+    # 배열이 빈 배열일 경우 -1을 채워서 리턴
