436cdaec93a4cc7644f174b924cde5939a163078
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-11 10:25:05 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,4 +1,6 @@
 def solution(n):
-    ls = list(str(int(n))) #정수형 n을 문자열로 변환한뒤 ls의 리스트에 저장
-    ls.sort(reverse = True) #리스트를 내림차순으로 정렬
-    return int("".join(ls)) #join으로 합친후 정수형 변환
+    n = str(n)
+    n = list(map(int,list(n)))
+    n = sorted(n,reverse=True)
+    
+    return int("".join(map(str,n)))
