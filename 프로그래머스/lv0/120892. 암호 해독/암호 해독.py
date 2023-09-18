0fe000a00814c69bf8b09847df49ca9157f5686c
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-18 11:03:51 +0900

    [level 0] Title: 암호 해독, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,6 @@
 def solution(cipher, code):
     answer = ''
-    
+    #cipher code의 배수 만큼이 진짜 글자
     for i in range(code,len(cipher)+1):
         if i % code == 0:
             answer += cipher[i-1]
