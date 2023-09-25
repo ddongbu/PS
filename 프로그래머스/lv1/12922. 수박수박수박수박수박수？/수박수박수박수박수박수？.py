4c5f27a842d6b0dac06483628cf7455285659d62
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-09-25 09:36:59 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,3 +1,8 @@
 def solution(n):
-    str = "수박" * n
-    return str[:n]
+    answer = ''
+    for i in range(0,n):
+        if i % 2 == 0:
+            answer += "수"
+        else:
+            answer += "박"
+    return answer
