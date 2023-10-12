2777f283c893425caa74179ed951d20434620485
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-10-12 20:55:34 +0900

    [level 2] Title: 숫자의 표현, Time: 3.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,11 @@
 def solution(n):
     #n보다 작은 자연수들의 합이 n과 같은 것
-    
     result = 1
-    #자기자신도 세기 때문에
-    for i in range(1, n // 2 + 1):
-        #n의 중반까지 순회
+    for i in range(1, n//2 + 1):
         total = 0
         while total < n:
             total += i
-            if total == n:
+            if total == n :
                 result += 1
                 break
             i += 1
