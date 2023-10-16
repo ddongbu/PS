e77add4810cad315e1e07f3e063a85c8310ec5da
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-10-16 10:02:03 +0900

    [Silver II] Title: 부분수열의 합, Time: 288 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,14 +1,28 @@
 import sys
-from itertools import combinations
 
 input = sys.stdin.readline
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
-for i in range(1, n+1):
-    comb = combinations(arr, i) #배열에서 뽑을 수 있는 모든 조합을 구해준다.
-    for x in comb:
-        if sum(x) == s: #총합이 구해고자 하는 합과 같다면
-            cnt += 1 #cnt +
 
+
+def bfs(idx, sub_sum):
+    global cnt
+
+    if idx >= n:  # 백 트래킹
+        return
+
+    sub_sum += arr[idx]
+
+    if sub_sum == s:
+        cnt += 1
+
+    # 현재 arr[idx]를 선택한 경우의 가지
+    bfs(idx + 1, sub_sum)
+
+    # 현재 arr[idx]를 선택하지 않은 경우의 가지
+    bfs(idx + 1, sub_sum - arr[idx])
+
+
+bfs(0, 0)
 print(cnt)
