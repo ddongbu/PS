795a428874f360b06564a8d858e7a07e1967e004
Sangmin Lee <105434827+ddongbu@users.noreply.github.com>
2023-10-25 17:41:52 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,7 @@
+select animal_ins.animal_id , animal_outs.name
+from animal_ins
+left join animal_outs
+on animal_ins.animal_id = animal_outs.animal_id 
+where animal_ins.datetime > animal_outs.datetime
+order by animal_ins.datetime
+
